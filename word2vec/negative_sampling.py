670fecaf06332c6ea561cc7591d6a7eddfb2c280
codezzzsleep <codezzzsleep@163.com>
2023-08-26 14:45:37 +0800

    更新负采样技术优化代码

--- word2vec/negative_sampling.py
+++ word2vec/negative_sampling.py
@@ -1,125 +1,143 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader
-from collections import defaultdict
+from torch.utils.data import DataLoader, Dataset
+from collections import Counter
 import numpy as np
-import torch.nn.functional as F
+import matplotlib.pyplot as plt
+from torch.utils.tensorboard import SummaryWriter
 
 
-# 数据预处理
-def tokenize_corpus(corpus):
-    tokens = [x.split() for x in corpus]
-    return tokens
+class SkipGramDataset(Dataset):
+    def __init__(self, text, window_size=2):
+        tokenized_text = text.split()
+        word_counts = Counter(tokenized_text)
+        self.word_to_idx = {word: idx for idx, (word, _) in enumerate(word_counts.items())}
+        self.idx_to_word = {idx: word for word, idx in self.word_to_idx.items()}
 
+        word_freqs = np.asarray(sorted(word_counts.values(), reverse=True))
+        self.word_probs = word_freqs / word_freqs.sum()
 
-def create_vocabulary(tokenized_corpus):
-    vocabulary = defaultdict(int)
-    for sentence in tokenized_corpus:
-        for token in sentence:
-            vocabulary[token] += 1
-    return vocabulary
+        self.window_size = window_size
+        self.data = []
 
-
-def create_context_word_pairs(tokenized_corpus, vocabulary, window_size=2):
-    idx_to_word = {i: word for i, word in enumerate(vocabulary)}
-    word_to_idx = {word: i for i, word in enumerate(vocabulary)}
-
-    pairs = []
-    for sentence in tokenized_corpus:
-        indices = [word_to_idx[word] for word in sentence]
-        for center_word_pos in range(len(indices)):
-            for w in range(-window_size, window_size + 1):
-                context_word_pos = center_word_pos + w
-                if context_word_pos < 0 or context_word_pos >= len(indices) or center_word_pos == context_word_pos:
+        for idx, center_word in enumerate(tokenized_text):
+            for offset in range(-window_size, window_size + 1):
+                if offset == 0:
                     continue
-                pairs.append((indices[center_word_pos], indices[context_word_pos]))
-    return pairs, word_to_idx, idx_to_word
 
+                context_idx = idx + offset
+                if context_idx < 0 or context_idx >= len(tokenized_text):
+                    continue
 
-# 更大的语料库
-corpus = [
-    "我爱自然语言处理",
-    "自然语言处理是非常有趣的",
-    "人工智能领域与自然语言处理密切相关",
-    "深度学习技术在自然语言处理中有广泛应用",
-    "最近几年自然语言处理取得了显著进展",
-]
-
-tokenized_corpus = tokenize_corpus(corpus)
-vocabulary = create_vocabulary(tokenized_corpus)
-pairs, word_to_idx, idx_to_word = create_context_word_pairs(tokenized_corpus, vocabulary, window_size=2)
-
-
-def negative_sampling_distribution(vocabulary):
-    word_freqs = np.array([freq for _, freq in vocabulary.items()], dtype=np.float64)
-    unigram_distribution = word_freqs / np.sum(word_freqs)
-    return torch.tensor(unigram_distribution ** (3 / 4))
-
-
-class NegativeSamplingWord2VecDataset(Dataset):
-    def __init__(self, pairs, neg_dist, num_neg_samples=5):
-        self.pairs = pairs
-        self.neg_dist = neg_dist
-        self.num_neg_samples = num_neg_samples
+                context_word = tokenized_text[context_idx]
+                self.data.append((center_word, context_word))
 
     def __len__(self):
-        return len(self.pairs)
+        return len(self.data)
 
     def __getitem__(self, idx):
-        center, context = self.pairs[idx]
-        neg_samples = torch.multinomial(self.neg_dist, self.num_neg_samples, replacement=True)
-        return center, context, neg_samples
+        center_word, context_word = self.data[idx]
+        center_idx = self.word_to_idx[center_word]
+        context_idx = self.word_to_idx[context_word]
+        return center_idx, context_idx
 
 
-neg_dist = negative_sampling_distribution(vocabulary)
-dataset = NegativeSamplingWord2VecDataset(pairs, neg_dist)
+class Word2Vec(nn.Module):
+    def __init__(self, vocab_size, embed_size, negative_samples=5):
+        super(Word2Vec, self).__init__()
+        self.in_embedding = nn.Embedding(vocab_size, embed_size)
+        self.out_embedding = nn.Linear(embed_size, vocab_size)
+        self.negative_samples = negative_samples
 
+    def negative_sampling(self, scores, context_idx, word_probs):
+        n = self.negative_samples
+        n_samples = torch.tensor(
+            np.random.choice(len(self.word_to_idx), (scores.shape[0], n), replace=True, p=word_probs)).to(device)
 
-# 构建模型
-class NegativeSamplingWord2Vec(nn.Module):
-    def __init__(self, vocab_size, embed_size):
-        super(NegativeSamplingWord2Vec, self).__init__()
-        self.in_embed = nn.Embedding(vocab_size, embed_size)
-        self.out_embed = nn.Embedding(vocab_size, embed_size)
+        out_embeddings = self.out_embedding.weight
 
-    def forward(self, center, context, neg_samples):
-        center_embed = self.in_embed(center)
-        context_embed = self.out_embed(context)
-        neg_embed = self.out_embed(neg_samples)
+        true_scores = torch.gather(scores, 1, context_idx.unsqueeze(1)).squeeze()
+        true_scores_term = torch.log(torch.sigmoid(true_scores))
 
-        positive_score = torch.bmm(center_embed.unsqueeze(1), context_embed.unsqueeze(2)).squeeze()
-        negative_score = torch.bmm(center_embed.unsqueeze(1), neg_embed.permute(0, 2, 1)).squeeze()
+        negative_scores = scores.gather(1, n_samples)
+        negative_scores_term = torch.sum(torch.log(torch.sigmoid(-1 * negative_scores)), dim=1)
 
-        loss = -torch.sum(F.logsigmoid(positive_score) + torch.sum(F.logsigmoid(-negative_score), dim=1))
+        return -torch.mean(true_scores_term + negative_scores_term)
+
+    def forward(self, center_idx, context_idx, word_probs):
+        center_embed = self.in_embedding(center_idx)
+        scores = self.out_embedding(center_embed)
+        if self.negative_samples > 0:
+            loss = self.negative_sampling(scores, context_idx, word_probs)
+        else:
+            loss = criterion(scores, context_idx)
         return loss
 
 
-vocab_size = len(vocabulary)
-embed_size = 100
-model = NegativeSamplingWord2Vec(vocab_size, embed_size)
+loss_list = []
+writer = SummaryWriter("runs/logs")
+min_loss = 10000.0
+criterion = nn.CrossEntropyLoss()
 
-# 训练模型
-epochs = 100
-batch_size = 64
-learning_rate = 1e-3
+
+def train(model, data_loader, dataset, epochs, learning_rate):
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    for epoch in range(epochs):
+        run_loss = 0.0
+        for batch, (center_idx, context_idx) in enumerate(data_loader):
+            center_idx = center_idx.to(device)
+            context_idx = context_idx.to(device)
+
+            # 修改这一行，将上下文索引和词频分布作为输入
+            loss = model(center_idx, context_idx, dataset.word_probs)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            run_loss += loss.item()
+            if batch % 500 == 0:
+                print(f"Epoch: {epoch}, Batch: {batch}, Loss: {loss.item()}")
+                writer.add_scalar("loss/500_batch", loss.item(), batch / 500)
+
+                if loss.item() < min_loss:
+                    torch.save(model.state_dict(), 'runs/best_model.pt')
+
+        loss_list.append(run_loss / (batch + 1))
+        writer.add_scalar("meansLoss/epoch", run_loss / (batch + 1), epoch)
+
+
+file_path = 'data/text8.train.txt'
+with open(file_path, 'r', encoding='utf-8') as file:
+    text = file.read()
+
+window_size = 2
+embed_size = 50
+negative_samples = 5
+batch_size = 4
+epochs = 1
+learning_rate = 0.01
+
+dataset = SkipGramDataset(text, window_size=window_size)
+data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-dataloader = DataLoader(dataset, shuffle=True, batch_size=batch_size)
+vocab_size = len(dataset.word_to_idx)
+model = Word2Vec(vocab_size, embed_size, negative_samples)
+model.to(device)
 
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-model = model.to(device)
+train(model, data_loader, dataset, epochs, learning_rate)
+print("train done!")
+torch.save(model.state_dict(), 'runs/last_model.pt')
+writer.close()
+x = list(range(1, epochs + 1))
+plt.plot(x, loss_list)
 
-for epoch in range(epochs):
-    losses = []
-    for batch in dataloader:
-        center, context, neg_samples = batch
-        center, context, neg_samples = center.to(device), context.to(device), neg_samples.to(device)
+plt.title('loss/epoch')
+plt.xlabel('x-axis')
+plt.ylabel('y-axis')
 
-        optimizer.zero_grad()
-        loss = model(center, context, neg_samples)
-        loss.backward()
-        optimizer.step()
-        losses.append(loss.item())
-
-    print(f"Epoch {epoch + 1}, Loss: {np.mean(losses)}")
+plt.show()
+print("task done!")
