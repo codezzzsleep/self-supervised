46a6586d6533e93802dd305ced281ef0d91a2009
codezzzsleep <codezzzsleep@163.com>
2023-08-30 20:00:55 +0800

    word2vec skip and cbow

--- word2vec/demo.py
+++ word2vec/demo.py
@@ -1,119 +1,205 @@
 import torch
+from torch.autograd import Variable
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
-import torch.utils.data as data
 
-import numpy as np
-from collections import Counter
-import random
+torch.manual_seed(1)
+CONTEXT_SIZE = 2  # 2 words to the left, 2 to the right
+text = """We are about to study the idea of a computational process.
+Computational processes are abstract beings that inhabit computers.
+As they evolve, processes manipulate other abstract things called data.
+The evolution of a process is directed by a pattern of rules
+called a program. People create programs to direct processes. In effect,
+we conjure the spirits of the computer with our spells.""".split()
 
-# 超参数设置
-context_size = 2
-embedding_dim = 100
-epochs = 10
-batch_size = 64
+split_ind = (int)(len(text) * 0.8)
 
-# 你的语料库
-corpus = ['This', 'is', 'an', 'example', 'sentence']  # 替换为实际文本数据，如['This', 'is', 'an', 'example', 'sentence']
-
-
-# 构建词汇表
-def build_vocab(corpus, min_freq):
-    word_count = Counter(corpus)
-    vocab = [word for word, freq in word_count.items() if freq >= min_freq]
-    return vocab
-
-
-# 生成上下文词对
-def generate_context_pairs(corpus, context_size):
-    context_pairs = []
-    for idx in range(context_size, len(corpus) - context_size):
-        center_word = corpus[idx]
-        context_words = corpus[idx - context_size:idx] + corpus[idx + 1:idx + context_size + 1]
-        context_pairs.extend([(center_word, context) for context in context_words])
-    return context_pairs
-
-
-# Skip-gram模型
-class SkipGram(nn.Module):
-    def __init__(self, vocab_size, embedding_dim):
-        super(SkipGram, self).__init__()
-        self.center_embeddings = nn.Embedding(vocab_size, embedding_dim)
-        self.context_embeddings = nn.Embedding(vocab_size, embedding_dim)
-
-    def forward(self, center_word, context_word):
-        center_embeds = self.center_embeddings(center_word)
-        context_embeds = self.context_embeddings(context_word)
-
-        scores = torch.matmul(center_embeds, context_embeds.t())
-        return scores
-
-
-# CBOW模型
-class CBOW(nn.Module):
-    def __init__(self, vocab_size, embedding_dim):
-        super(CBOW, self).__init__()
-        self.embeddings = nn.Embedding(vocab_size, embedding_dim)
-
-    def forward(self, context_words):
-        embeds = torch.mean(self.embeddings(context_words), dim=1)
-        scores = torch.matmul(embeds, self.embeddings.weight.t())
-        return scores
-
-
-def train(model, dataloader, optimizer, loss_fn, device, vocab_size):
-    model.train()
-    for batch, (center_word, context_word) in enumerate(dataloader):
-        center_word, context_word = center_word.to(device), context_word.to(device)
-        optimizer.zero_grad()
-
-        if isinstance(model, SkipGram):
-            scores = model(center_word, context_word)
-            context_word = torch.tensor([context_word[i // vocab_size] for i in range(scores.size(0))],
-                                        dtype=torch.long)
-        elif isinstance(model, CBOW):
-            context_word_list = []
-            for idx, center in enumerate(center_word):
-                context_word_list.append(
-                    torch.tensor([context_word[item] for item in range(idx, context_word.size(0), len(center_word))],
-                                 dtype=torch.long).to(device))
-            context_words_batch = torch.stack(context_word_list)
-            scores = model(context_words_batch)
-
-        loss = loss_fn(scores, center_word if isinstance(model, CBOW) else context_word)
-        loss.backward()
-        optimizer.step()
-
-    # 数据准备
-
-
-vocab = build_vocab(corpus, min_freq=1)
-word2index = {word: i for i, word in enumerate(vocab)}
-index2word = {i: word for word, i in word2index.items()}
+# By deriving a set from `raw_text`, we deduplicate the array
+vocab = set(text)
 vocab_size = len(vocab)
+print('vocab_size:', vocab_size)
 
-context_pairs = generate_context_pairs(corpus, context_size)
-center_word_data = [word2index[pair[0]] for pair in context_pairs]
-context_word_data = [word2index[pair[1]] for pair in context_pairs]
+w2i = {w: i for i, w in enumerate(vocab)}
+i2w = {i: w for i, w in enumerate(vocab)}
 
-# 数据集准备
-dataset = data.TensorDataset(torch.tensor(center_word_data, dtype=torch.long),
-                             torch.tensor(context_word_data, dtype=torch.long))
-dataloader = data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
-# 模型、优化器、损失函数准备
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-skipgram_model = SkipGram(vocab_size, embedding_dim).to(device)
-cbow_model = CBOW(vocab_size, embedding_dim).to(device)
-optimizer_skipgram = optim.Adam(skipgram_model.parameters(), lr=0.001)
-optimizer_cbow = optim.Adam(cbow_model.parameters(), lr=0.001)
-loss_fn = nn.CrossEntropyLoss()
+# context window size is two
+def create_cbow_dataset(text):
+    data = []
+    for i in range(2, len(text) - 2):
+        context = [text[i - 2], text[i - 1],
+                   text[i + 1], text[i + 2]]
+        target = text[i]
+        data.append((context, target))
+    return data
 
-for epoch in range(epochs):
-    print(f"Epoch: {epoch + 1}")
 
-    print("Training Skip-gram model...")
-    train(skipgram_model, dataloader, optimizer_skipgram, loss_fn, device, vocab_size)
+def create_skipgram_dataset(text):
+    import random
+    data = []
+    for i in range(2, len(text) - 2):
+        data.append((text[i], text[i - 2], 1))
+        data.append((text[i], text[i - 1], 1))
+        data.append((text[i], text[i + 1], 1))
+        data.append((text[i], text[i + 2], 1))
+        # negative sampling
+        for _ in range(4):
+            if random.random() < 0.5 or i >= len(text) - 3:
+                rand_id = random.randint(0, i - 1)
+            else:
+                rand_id = random.randint(i + 3, len(text) - 1)
+            data.append((text[i], text[rand_id], 0))
+    return data
 
-    print("Training CBOW model...")
-    train(cbow_model, dataloader, optimizer_cbow, loss_fn, device, vocab_size)
+
+cbow_train = create_cbow_dataset(text)
+skipgram_train = create_skipgram_dataset(text)
+print('cbow sample', cbow_train[0])
+print('skipgram sample', skipgram_train[0])
+
+
+class CBOW(nn.Module):
+    def __init__(self, vocab_size, embd_size, context_size, hidden_size):
+        super(CBOW, self).__init__()
+        self.embeddings = nn.Embedding(vocab_size, embd_size)
+        self.linear1 = nn.Linear(2 * context_size * embd_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, vocab_size)
+
+    def forward(self, inputs):
+        embedded = self.embeddings(inputs).view((1, -1))
+        hid = F.relu(self.linear1(embedded))
+        out = self.linear2(hid)
+        log_probs = F.log_softmax(out, dim=1)
+        return log_probs
+
+
+class SkipGram(nn.Module):
+    def __init__(self, vocab_size, embd_size):
+        super(SkipGram, self).__init__()
+        self.embeddings = nn.Embedding(vocab_size, embd_size)
+
+    def forward(self, focus, context):
+        embed_focus = self.embeddings(focus).view((1, -1))
+        embed_ctx = self.embeddings(context).view((1, -1))
+        score = torch.mm(embed_focus, torch.t(embed_ctx))
+        log_probs = F.logsigmoid(score)
+
+        return log_probs
+
+
+embd_size = 100
+learning_rate = 0.001
+n_epoch = 30
+
+
+def train_cbow():
+    hidden_size = 64
+    losses = []
+    loss_fn = nn.NLLLoss()
+    model = CBOW(vocab_size, embd_size, CONTEXT_SIZE, hidden_size)
+    print(model)
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+
+    for epoch in range(n_epoch):
+        total_loss = .0
+        for context, target in cbow_train:
+            ctx_idxs = [w2i[w] for w in context]
+            ctx_var = Variable(torch.LongTensor(ctx_idxs))
+
+            model.zero_grad()
+            log_probs = model(ctx_var)
+
+            loss = loss_fn(log_probs, Variable(torch.LongTensor([w2i[target]])))
+
+            loss.backward()
+            optimizer.step()
+
+            total_loss += loss.data.item()
+        losses.append(total_loss)
+    return model, losses
+
+
+def train_skipgram():
+    losses = []
+    loss_fn = nn.MSELoss()
+    model = SkipGram(vocab_size, embd_size)
+    print(model)
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+
+    for epoch in range(n_epoch):
+        total_loss = .0
+        for in_w, out_w, target in skipgram_train:
+            in_w_var = Variable(torch.LongTensor([w2i[in_w]]))
+            out_w_var = Variable(torch.LongTensor([w2i[out_w]]))
+
+            model.zero_grad()
+            log_probs = model(in_w_var, out_w_var)
+            loss = loss_fn(log_probs[0], Variable(torch.Tensor([target])))
+
+            loss.backward()
+            optimizer.step()
+
+            total_loss += loss.data.item()
+        losses.append(total_loss)
+    return model, losses
+
+
+cbow_model, cbow_losses = train_cbow()
+sg_model, sg_losses = train_skipgram()
+
+
+# test
+# You have to use other dataset for test, but in this case I use training data because this dataset is too small
+def test_cbow(test_data, model):
+    print('====Test CBOW===')
+    correct_ct = 0
+    for ctx, target in test_data:
+        ctx_idxs = [w2i[w] for w in ctx]
+        ctx_var = Variable(torch.LongTensor(ctx_idxs))
+
+        model.zero_grad()
+        log_probs = model(ctx_var)
+        _, predicted = torch.max(log_probs.data, 1)
+        predicted_word = i2w[predicted.item()]
+        print('predicted:', predicted_word)
+        print('label    :', target)
+        if predicted_word == target:
+            correct_ct += 1
+
+    print('Accuracy: {:.1f}% ({:d}/{:d})'.format(correct_ct / len(test_data) * 100, correct_ct, len(test_data)))
+
+
+def test_skipgram(test_data, model):
+    print('====Test SkipGram===')
+    correct_ct = 0
+    for in_w, out_w, target in test_data:
+        in_w_var = Variable(torch.LongTensor([w2i[in_w]]))
+        out_w_var = Variable(torch.LongTensor([w2i[out_w]]))
+
+        model.zero_grad()
+        log_probs = model(in_w_var, out_w_var)
+        _, predicted = torch.max(log_probs.data, 1)
+        predicted = predicted[0]
+        if predicted == target:
+            correct_ct += 1
+
+    print('Accuracy: {:.1f}% ({:d}/{:d})'.format(correct_ct / len(test_data) * 100, correct_ct, len(test_data)))
+
+
+test_cbow(cbow_train, cbow_model)
+print('------')
+test_skipgram(skipgram_train, sg_model)
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def showPlot(points, title):
+    plt.figure()
+    fig, ax = plt.subplots()
+    plt.plot(points)
+
+
+showPlot(cbow_losses, 'CBOW Losses')
+showPlot(sg_losses, 'SkipGram Losses')
--- word2vec/main.py
+++ word2vec/main.py
@@ -5,19 +5,18 @@ import utils
 from net import Word2Vec
 from train import train
 
-writer = SummaryWriter("runs/logs")
-
-file_path = 'data/text8.train.txt'
-with open(file_path, 'r', encoding='utf-8') as file:
-    # 读取文件内容
-    text = file.read()
-
 # 超参数设置
 window_size = 2
 embed_size = 50
 batch_size = 4
 epochs = 1
 learning_rate = 0.01
+# tensorboard 可视化训练过程
+writer = SummaryWriter("runs/logs")
+
+file_path = 'data/text8'
+with open(file_path, 'r', encoding='utf-8') as file:
+    text = file.read()
 
 dataset, data_loader = utils.load_data(text)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -30,14 +29,11 @@ loss_list = train(model, data_loader, epochs, learning_rate, device, writer)
 print("train done!")
 torch.save(model.state_dict(), 'runs/last_model.pt')
 writer.close()
+
 x = list(range(1, epochs + 1))
 plt.plot(x, loss_list)
-
-# 添加标题和坐标轴标签
 plt.title('loss/epoch')
 plt.xlabel('x-axis')
 plt.ylabel('y-axis')
-
-# 显示图形
 plt.show()
 print("task done!")
