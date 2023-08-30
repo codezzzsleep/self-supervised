18c1350a9b0851ee83ad3191c8b60f826255c2ed
codezzzsleep <codezzzsleep@163.com>
2023-08-30 19:26:55 +0800

    word2vec

--- word2vec/gensim/word2vec-gensim.py
+++ word2vec/gensim/word2vec-gensim.py
@@ -3,8 +3,8 @@ from gensim.models import word2vec
 sentences = word2vec.Text8Corpus('text8')
 
 model = word2vec.Word2Vec(sentences,
-                          sg=1,
-                          size=100,
+                          sg=1,  # sg: 指示要使用 Skip-gram(设置为1) 还是 CBOW(设置为0) 模型。
+                          vector_size=100,
                           window=5,
                           min_count=5,
                           negative=3,
