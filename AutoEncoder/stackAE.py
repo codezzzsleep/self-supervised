f8e117f188053dc6608dafa272d392fd3638ae7e
codezzzsleep <codezzzsleep@163.com>
2023-08-31 10:57:59 +0800

    stackAE

--- AutoEncoder/stackAE.py
+++ AutoEncoder/stackAE.py
@@ -3,11 +3,12 @@ import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 
 # 超参数
-batch_size = 100
+batch_size = 256
 learning_rate = 0.001
-num_epochs = 20
+num_epochs = 200
 
 # 数据处理
 transform = transforms.Compose([
@@ -51,22 +52,31 @@ model = StackedAutoencoder()
 # 损失函数和优化器
 criterion = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+writer = SummaryWriter("runs/stack_log")
 
+step = 2
 # 训练模型
 for epoch in range(num_epochs):
+    running_loss = 0.0
     for i, (images, _) in enumerate(train_loader):
         images = images.view(images.size(0), -1)
 
         # 前向传播
         outputs = model(images)
         loss = criterion(outputs, images)
-
+        running_loss += loss.item()
         # 反向传播
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-        if (i + 1) % 100 == 0:
-            print(f'Epoch [{epoch + 1}/{num_epochs}], Step [{i + 1}/{len(train_loader)}], Loss: {loss.item():.4f}')
-
+        # if (i + 1) % 100 == 0:
+        #     print(f'Epoch [{epoch + 1}/{num_epochs}], Step [{i + 1}/{len(train_loader)}], Loss: {loss.item():.4f}')
+        if epoch == step:
+            writer.add_image(f"imgae_{step}", images[23].view(1, 28, 28), step)
+            writer.add_image(f"output_image_{step}", outputs[23].view(1, 28, 28), step)
+            step = step * 2
+    writer.add_scalar("Loss_epoch/train", running_loss / i, epoch + 1)
+    print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}')
+writer.close()
 print('Training completed.')
